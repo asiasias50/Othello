254f19a6116dae7b55e02392bd0afdc143a1835f
Alexey <asiasias50@gmail.com>
2021-09-02 23:03:12 +0100

    Client and Server are implemented with user registration and authorisation.

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 import socket
 from json import dumps, loads
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((socket.gethostname(), 1234))
 
-user = "ty"
-password = "0987"
+class Client:
+    CREATE_ACCOUNT = "ca"
+    SIGN_IN = "si"
 
-client.send(bytes("qs" + dumps((user, password)), "utf-8"))
+    def __init__(self):
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__client.connect((socket.gethostname(), 1234))
+
+    def create_account(self, username, password):
+        self.__client.send(bytes(self.CREATE_ACCOUNT + dumps((username, password)), "utf-8"))
+        return loads(self.__client.recv(1024).decode('utf-8'))
+
+    def sign_in(self, username, password):
+        self.__client.send(bytes(self.SIGN_IN + dumps((username, password)), "utf-8"))
+        return loads(self.__client.recv(1024).decode('utf-8'))
+
+
+if __name__ == '__main__':
+    c = Client()
+    print(c.sign_in("sum", "yyyIII"))
--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from json import dumps, loads, dump, load
 
 
 class Server:
+    CREATE_ACCOUNT = "ca"
+    SIGN_IN = "si"
 
     def __init__(self):
-        self.__procedure_list = {"ca": self.__create_account}
+        self.__procedure_list = {self.CREATE_ACCOUNT: self.__create_account, self.SIGN_IN: self.__sign_in}
         self.__user_information = None
         try:
             with open("Users.txt", "r") as file:
@@ -18,17 +20,14 @@ class Server:
         self.__server.bind((socket.gethostname(), 1234))
         self.__server.listen(10)
         self.__message_cycle()
-        with open("Users.txt", "w") as file:
-            dump(self.__user_information, file)
 
     def __message_cycle(self):
         while True:
             client_socket, address = self.__server.accept()
             message = client_socket.recv(1024).decode("utf-8")
-            if message[:2] != "qs":
-                self.__procedure_list[message[:2]](loads(message[2:]), client_socket, address)
-            else:
-                break
+            self.__procedure_list[message[:2]](loads(message[2:]), client_socket, address)
+            with open("Users.txt", "w") as file:
+                dump(self.__user_information, file)
 
     def __create_account(self, arguments, client, address):
         username, password = arguments
@@ -37,6 +36,17 @@ class Server:
         else:
             message = True
             self.__user_information[username] = password
+        client.send(bytes(dumps(message), "utf-8"))
+
+    def __sign_in(self, arguments, client, address):
+        username, password = arguments
+        if username in self.__user_information:
+            if self.__user_information[username] == password:
+                message = 1
+            else:
+                message = 0
+        else:
+            message = -1
         client.send(bytes(dumps(message), "utf-8"))
 
 
